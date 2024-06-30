4df9ef965510d1b7940405efc6069fda356d29f2
Rafael Herik <rafaelherik@gmail.com>
2024-06-30 21:33:13 +0200

    Health check and documentation last adjustments.

--- health_check/health_check.sh
+++ health_check/health_check.sh
@@ -2,9 +2,12 @@
 
 
 LOG_FILE="/var/data/log/health_check.log"
+
 http_response=$(curl -s -o /dev/null -w "%{http_code}" "$SERVICE_URL")
 if [ "$http_response" == "200" ]; then
     echo "$(date): API at $SERVICE_URL is reachable - Health check passed" >> "$LOG_FILE"
 else
     echo "$(date): API at $SERVICE_URL is unreachable - Health check failed (HTTP $http_response)" >> "$LOG_FILE"
 fi
+
+cat /var/data/log/health_check.log
